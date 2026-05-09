3463bd0e7b93b9becfdb50da6b45ecf0b402d2fe
Amer Kriany <amer.kriany0@gmail.com>
2026-05-10 01:15:06 +0300

    feat: add orderEntety & orderModel

---
+++ lib/features/orders/data/models/orderModel.dart
@@ -0,0 +1,25 @@
+class Ordermodel {
+  final String restaurantId;
+  final String addressId;
+  final String paymentMethod;
+  final String? notes;
+  final String? couponCode;
+
+  Ordermodel({
+    required this.addressId,
+    required this.restaurantId,
+    required this.paymentMethod,
+    this.couponCode,
+    this.notes,
+  });
+
+  factory Ordermodel.fromJson(Map<String, dynamic> json) {
+    return Ordermodel(
+      addressId: json['restaurant_id'],
+      restaurantId: json['address_id'],
+      paymentMethod: json['payment_method'],
+      notes: json['notes'],
+      couponCode: json['coupon_code'],
+    );
+  }
+}
---
+++ lib/features/orders/domain/entity/orderEntity.dart
@@ -0,0 +1,15 @@
+class OrderEntity {
+  final String restaurantId;
+  final String addressId;
+  final String paymentMethod;
+  final String? notes;
+  final String? couponCode;
+  
+  OrderEntity({
+    required this.addressId,
+    required this.restaurantId,
+    required this.paymentMethod,
+    this.couponCode,
+    this.notes,
+  });
+}
